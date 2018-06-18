d7a511987901f1ed27465d7f1e5c9718bcbb7ca7
Nasma <nasma.mlamali@icloud.com>
2018-06-18 15:59:13 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Instanews
+
+
+
+## Presentation
+
+Instanews is a a news website fetching articles from the New-Yoirk Times 
+
+The purpose of this website, is to showcase the skills developped during class.
+
+##Tools
+
+this website is powered by:
+Javascript
+New-York times Api
+Css
+HTML
+
+
+## Install
+
+No instalation needed. The website should be compatible with most major browsers .
+
+
+## License
+
+No license.
+
+
---
+++ js/main.js
@@ -0,0 +1,98 @@
+$('select').change(function () {
+
+  var select = $('select').val();
+
+  var url = 'https://api.nytimes.com/svc/topstories/v2/' + select + '.json';
+  url += '?' + $.param({
+    'api-key': 'b004981c2f014cbaa934004bdf7f33b3'
+  });
+
+
+  $.ajax({
+    url: url,
+    method: 'GET',
+
+    //show loading gif
+
+    beforeSend: function () {
+      $('.loading').append('<img src=\'/images/ajax-loader.gif\' />').css({
+        "display": "flex",
+        "width": "100%"
+      });
+    },
+
+    success: function (data) {
+
+      // first one for testing purposes 
+      // $('.loading').append('<img src=\'/images/ajax-loader.gif\' />').css({"display": "flex", "width": "100%"});
+
+
+      $('.stories').empty();
+
+      //show stories
+
+      for (var i = 0; i < 12; ++i) {
+        if (data.results[i].multimedia[4]) {
+          $('.stories').append('<a href=\'' + data.results[i].url + '\' class=\'newslink\'><div class=\'newsbackground\' style=\'background-image: url(' + data.results[i].multimedia[4].url + ')\'> <p class = \'newstitle\'>' + data.results[i].abstract + '</p></div></a>');
+
+
+        } else {
+          data.results.splice(i, 1);
+          i--;
+        }
+        // console.log(data.results[i].multimedia);
+
+
+      }
+
+    },
+
+
+
+  });
+
+
+
+
+})
+
+
+var select = $('select').val();
+
+$('select').change(function () {
+
+  if ($('header').width() < 600) {
+
+    $('.logo').animate({
+
+      width: '19rem',
+      height: 'auto',
+    });
+  } else {
+
+    $('.logo').animate({
+
+      width: '5rem',
+      // marginTop: '-5rem',
+      marginLeft: '0rem',
+      height: 'auto',
+
+
+    });
+
+  }
+
+
+});
+
+
+
+$('select').change(function () {
+  $('header').animate({
+
+    height: 'auto',
+    marginLeft: '0rem',
+    marginTop: '1rem',
+
+  });
+});
